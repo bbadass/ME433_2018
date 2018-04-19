30086c33ec706c147ed4197c834b0640560c8cce
bbadass <biancabaldassarri2022@u.northwestern.edu>
2018-04-19 17:53:07 -0500

    starting code

---
+++ HW4/code_1.X/main.c
@@ -0,0 +1,133 @@
+#include<xc.h>           // processor SFR definitions
+#include<sys/attribs.h>  // __ISR macro
+
+// DEVCFG0
+#pragma config DEBUG = OFF // no debugging
+#pragma config JTAGEN = OFF // no jtag
+#pragma config ICESEL =  ICS_PGx1 // use PGED1 and PGEC1
+#pragma config PWP = OFF // no write protect
+#pragma config BWP = OFF // no boot write protect
+#pragma config CP = OFF // no code protect
+
+// DEVCFG1
+#pragma config FNOSC = PRIPLL // use primary oscillator with pll
+#pragma config FSOSCEN = OFF // turn off secondary oscillator
+#pragma config IESO = OFF // no switching clocks
+#pragma config POSCMOD = HS // high speed crystal mode
+#pragma config OSCIOFNC = OFF // disable secondary osc
+#pragma config FPBDIV = DIV_1 // divide sysclk freq by 1 for peripheral bus clock
+#pragma config FCKSM = CSDCMD // do not enable clock switch
+#pragma config WDTPS = PS1048576 // use slowest wdt
+#pragma config WINDIS = OFF // wdt no window mode
+#pragma config FWDTEN = OFF // wdt disabled
+#pragma config FWDTWINSZ = WINSZ_25 // wdt window at 25%
+
+// DEVCFG2 - get the sysclk clock to 48MHz from the 8MHz crystal
+#pragma config FPLLIDIV = DIV_2 // divide input clock to be in range 4-5MHz
+#pragma config FPLLMUL = MUL_24 // multiply clock after FPLLIDIV
+#pragma config FPLLODIV = DIV_2 // divide clock after FPLLMUL to get 48MHz
+#pragma config UPLLIDIV = DIV_2 // divider for the 8MHz input clock, then multiplied by 12 to get 48MHz for USB
+#pragma config UPLLEN = ON // USB clock on
+
+// DEVCFG3
+#pragma config USERID = 00000000 // some 16bit userid, doesn't matter what
+#pragma config PMDL1WAY = OFF // allow multiple reconfigurations
+#pragma config IOL1WAY = OFF // allow multiple reconfigurations
+#pragma config FUSBIDIO = ON // USB pins controlled by USB module
+#pragma config FVBUSONIO = ON // USB BUSON controlled by USB module
+
+
+#define CS LATAbits.LATA0
+#define SCK LATBbits.LATB14
+#define SDOI LATAbits.LATA1
+
+
+// function to send a byte via SPI and return the response
+    unsigned char spi_io(unsigned char o) {
+  SPI4BUF = o;
+  while(!SPI4STATbits.SPIRBF) { // wait to receive the byte
+    ;
+  }
+  return SPI4BUF;
+    }
+      
+    
+    //function to set the voltage 
+    void setVoltage (char a, int v) {
+    unsigned short t;
+    
+    t = a<< 15;
+    
+    t = t | 0b0111000000000000; //1st 8 bit number describing the channel is four set identifiers plus 4 user chosen ones
+    t = t | ((v&0b1111111111)<<2); //2nd 8 bit number describing the voltage v is a 10 bit string shifted back by two bits 
+    
+    CS=0; //lower the chip select line and enable the MCP
+    spi_io(t<<8); //shift
+    spi_io(t&0xFF); //shift
+    CS = 1; //disable the MCP
+    }
+
+   //function to initialize spin communication
+    void spi_init() {
+  // the chip select pin is used by the sram to indicate when a command is beginning (clear CS to low) and when it is ending (set CS high)
+  TRISAbits.TRISA0 = 0; // set up the chip select pin as an output
+  CS = 1; //no communication
+
+  // Master - SPI4, pins are: SDI4(F4), SDO4(F5), SCK4(F13).  
+  // we manually control SS4 as a digital output (F12)
+  // since the pic is just starting, we know that spi is off. We rely on defaults here
+ 
+  // setup spi4
+  SPI4CON = 0;              // turn off the spi module and reset it
+  SPI4BUF;                  // clear the rx buffer by reading from it
+  SPI4BRG = 0x3;            // baud rate to 10 MHz [SPI4BRG = (80000000/(2*desired))-1]
+  SPI4STATbits.SPIROV = 0;  // clear the overflow bit
+  SPI4CONbits.CKE = 1;      // data changes when clock goes from hi to lo (since CKP is 0)
+  SPI4CONbits.MSTEN = 1;    // master operation
+  SPI4CONbits.ON = 1;       // turn on spi 4
+
+                            // send a ram set status command.
+  CS = 0;                   // enable the ram
+  spi_io(0x01);             // ram write status
+  spi_io(0x41);             // sequential mode (mode = 0b01), hold disabled (hold = 0)
+  CS = 1;                   // finish the command
+}
+    
+int main() {
+
+    __builtin_disable_interrupts();
+
+    // set the CP0 CONFIG register to indicate that kseg0 is cacheable (0x3)
+    __builtin_mtc0(_CP0_CONFIG, _CP0_CONFIG_SELECT, 0xa4210583);
+
+    // 0 data RAM access wait states
+    BMXCONbits.BMXWSDRM = 0x0;
+
+    // enable multi vector interrupts
+    INTCONbits.MVEC = 0x1;
+
+    // disable JTAG to get pins back
+    DDPCONbits.JTAGEN = 0;
+
+    // Set pins to input and outputs, and their initial values
+    TRISBbits.TRISB4=1; //B4 is input (from push button)
+    TRISAbits.TRISA4=0; //A4 is output (to LED)
+    
+    LATAbits.LATA4=1; //A4 outs 3.3V    
+    
+
+    spi_init();
+    
+    __builtin_enable_interrupts();
+    
+    
+    while(1) {
+        _CP0_SET_COUNT(0); // PIC timing set to zero  
+        
+        setVoltage(0,512); //channel A is a sine wave
+        setVoltage(0,512/2); //channel B is a triangle wave        
+        
+         while(_CP0_GET_COUNT()< 24000){ //while loop with frequency 1kHz
+                      }
+    }
+}
