4dadfa51abf2dd4985f71ac0b910ce7f4bd093af
bbadass <biancabaldassarri2022@u.northwestern.edu>
2018-05-03 16:35:34 -0500

    works now!

--- HW9/code_6/firmware/src/app.c
+++ HW9/code_6/firmware/src/app.c
@@ -49,7 +49,9 @@ SUBSTITUTE GOODS, TECHNOLOGY, SERVICES, OR ANY CLAIMS BY THIRD PARTIES
 // *****************************************************************************
 
 #include "app.h"
-#include <stdio.h>
+#include <ST7735.h> //lcd library
+#include<stdio.h> //sprintf
+#include<i2c_master_noint.h>
 #include <xc.h>
 
 // *****************************************************************************
@@ -82,6 +84,211 @@ APP_DATA appData;
 // Section: Application Callback Functions
 // *****************************************************************************
 // *****************************************************************************
+
+void writeCharacter(unsigned short x, unsigned short y, char letter, unsigned short color_on, unsigned short color_off) {
+
+    unsigned short xi = 0;
+    unsigned short yi = 0;
+
+    for (xi = 0; xi < 5; xi++) { //loop over the five pixels (in x) per character
+
+        unsigned char pixels = ASCII[letter - 0x20][xi];
+
+        for (yi = 0; yi < 8; yi++) { //loop over bits of each character (8 pixels in y)
+            if ((pixels >> yi) & 1) { // if the yith bit of the ascii (shift back by yi) is on, turn the pixel on
+                LCD_drawPixel(x + xi, y + yi, color_on);
+            } else { // if the yith bit of the ascii (shift back by yi) is off, color the pixel of the off color
+                LCD_drawPixel(x + xi, y + yi, color_off);
+            }
+        }
+    }
+
+}
+
+//function to write messages composed of multiple characters on the LCD
+
+void writeString(unsigned short x, unsigned short y, unsigned char* words, unsigned short color_on, unsigned short color_off) {
+
+    unsigned int i = 0;
+
+    while (words[i]) { //run until there is a character, which works because sprintf has a null zero as its last character 
+        writeCharacter(x + 5 * i, y, words[i], color_on, color_off); //shift by 5 pixels per letter
+        i++;
+    }
+
+}
+
+
+//function to draw bars on the LCD
+
+void drawBarO(unsigned short x, unsigned short y, signed short n1o, unsigned short color_on, unsigned short color_off, unsigned int width, unsigned int length, signed short n1max) {
+
+    unsigned short ni1 = 0;
+    unsigned short ni2 = 0;
+    unsigned short yi = 0;
+    signed short length_max_v = length / 2;
+    signed short n1 = n1o / n1max;
+
+    for (yi = 0; yi <= width; yi++) { //loop over length of the bar
+
+        if (n1 > 0) {
+            for (ni2 = 0; ni2 <= length_max_v; ni2++) { //loop over n2 values of n (color off of the bar background)
+                LCD_drawPixel(x - ni2, y + yi, color_off);
+            }
+            if (n1 <= length_max_v) {
+                for (ni1 = 0; ni1 <= n1; ni1++) { //loop over n1 values of n (color on of the bar)
+                    LCD_drawPixel(x + ni1, y + yi, color_on);
+                }
+                for (ni2 = n1; ni2 <= length_max_v; ni2++) { //loop over n2 values of n (color off of the bar background)
+                    LCD_drawPixel(x + ni2, y + yi, color_off);
+                }
+            }
+            else {
+                for (ni1 = 0; ni1 <= length_max_v; ni1++) { //loop over n1 values of n (color on of the bar)
+                    LCD_drawPixel(x + ni1, y + yi, color_on);
+                }
+            }
+
+        }
+
+
+        if (n1 < 0) {
+            for (ni2 = 0; ni2 <= length_max_v; ni2++) { //loop over n2 values of n (color off of the bar background)
+                LCD_drawPixel(x + ni2, y + yi, color_off);
+            }
+            if (-n1 <= length_max_v) {
+                for (ni1 = 0; ni1 <= -n1; ni1++) { //loop over n1 values of n (color on of the bar)
+                    LCD_drawPixel(x - ni1, y + yi, color_on);
+                }
+                for (ni2 = -n1; ni2 <= length_max_v; ni2++) { //loop over n2 values of n (color off of the bar background)
+                    LCD_drawPixel(x - ni2, y + yi, color_off);
+                }
+            }
+            else {
+                for (ni1 = 0; ni1 <= length_max_v; ni1++) { //loop over n1 values of n (color on of the bar)
+                    LCD_drawPixel(x - ni1, y + yi, color_on);
+                }
+            }
+        }
+
+
+
+    }
+}
+
+void drawBarV(unsigned short x, unsigned short y, signed short n1o, unsigned short color_on, unsigned short color_off, unsigned int width, unsigned int length, signed short n1max) {
+
+    unsigned short ni1 = 0;
+    unsigned short ni2 = 0;
+    unsigned short xi = 0;
+    signed short length_max_v = length / 2;
+    signed short n1 = n1o / n1max;
+
+    for (xi = 0; xi <= width; xi++) { //loop over width of the bar
+
+        if (n1 > 0) {
+            for (ni2 = 0; ni2 <= length_max_v; ni2++) { //loop over n2 values of n (color off of the bar background)
+                LCD_drawPixel(x + xi, y - ni2, color_off);
+            }
+            if (n1 < length_max_v) {
+                for (ni1 = 0; ni1 <= n1; ni1++) { //loop over n1 values of n (color on of the bar)
+                    LCD_drawPixel(x + xi, y + ni1, color_on);
+                }
+                for (ni2 = n1; ni2 <= length_max_v; ni2++) { //loop over n2 values of n (color off of the bar background)
+                    LCD_drawPixel(x + xi, y + ni2, color_off);
+                }
+            }
+            else {
+                for (ni1 = 0; ni1 <= length_max_v; ni1++) { //loop over n1 values of n (color on of the bar)
+                    LCD_drawPixel(x + xi, y + ni1, color_on);
+                }
+            }
+
+        }
+
+
+        if (n1 < 0) {
+            for (ni2 = 0; ni2 <= length_max_v; ni2++) { //loop over n2 values of n (color off of the bar background)
+                LCD_drawPixel(x + xi, y + ni2, color_off);
+            }
+            if (-n1 <= length_max_v) {
+                for (ni1 = 0; ni1 <= -n1; ni1++) { //loop over n1 values of n (color on of the bar)
+                    LCD_drawPixel(x + xi, y - ni1, color_on);
+                }
+                for (ni2 = -n1; ni2 <= length_max_v; ni2++) { //loop over n2 values of n (color off of the bar background)
+                    LCD_drawPixel(x + xi, y - ni2, color_off);
+                }
+            }
+            else {
+                for (ni1 = 0; ni1 <= length_max_v; ni1++) { //loop over n1 values of n (color on of the bar)
+                    LCD_drawPixel(x + xi, y - ni1, color_on);
+                }
+            }
+        }
+
+
+
+    }
+}
+
+void write_i2c(unsigned char add, unsigned char reg, unsigned char val) {
+    i2c_master_start(); //begin the start sequence
+    i2c_master_send(add << 1 | 0); // send the slave address of the register, left shifted by 1, clearing last bit and setting it to 0 indicating to write
+    i2c_master_send(reg); //send the adress
+    i2c_master_send(val); //send the bits
+    i2c_master_stop(); //stop sequence
+
+}
+
+unsigned char read_i2c(unsigned char add, unsigned char reg) {
+    i2c_master_start(); //begin the start sequence
+    i2c_master_send(add << 1 | 0); // send the slave address, left shifted by 1, clearing last bit and setting it to 0 indicating to write
+    i2c_master_send(reg); //send the address of the general purpose i/o register
+    i2c_master_restart(); //restart
+    i2c_master_send(add << 1 | 1); // send the slave address, left shifted by 1, clearing last bit and setting it to 1 indicating to write
+    unsigned char r = i2c_master_recv(); //save the output of the slave
+    i2c_master_ack(1); //done talking to the chip (0 if not sone but just recieved))
+    i2c_master_stop(); //stop sequence
+    return r; //return the value read by the chip
+}
+
+unsigned char read_multiple_i2c(unsigned char add, unsigned char reg1, unsigned char* outs, unsigned int length) {
+    i2c_master_start(); //begin the start sequence
+    i2c_master_send(add << 1 | 0); // send the slave address, left shifted by 1, clearing last bit and setting it to 0 indicating to write
+    i2c_master_send(reg1); //send the address of the general purpose i/o register
+    i2c_master_restart(); //restart
+    i2c_master_send(add << 1 | 1); // send the slave address, left shifted by 1, clearing last bit and setting it to 1 indicating to write
+    unsigned int i = 0;
+    for (i = 0; i <= length; i++) { //loop over all reading registers
+        outs[i] = i2c_master_recv(); //save the output of the slave
+        if (i == length) { //when the loop reaches the last value
+            i2c_master_ack(1); //not done talking to the chip 
+        } else { //for every other value read out
+            i2c_master_ack(0); //done talking to the chip  
+        }
+    }
+    i2c_master_stop(); //stop sequence
+}
+
+//initialize LSM
+
+void initExp() {
+    ANSELBbits.ANSB2 = 0; //make B2 output digital
+    ANSELBbits.ANSB3 = 0; //make B3 output digital
+
+    i2c_master_setup();
+    write_i2c(0b1101011, 0x12, 0b00000100); //communicate with crl3_c and set ifing (6th bit) to 1, which allows to read multiple registers in a row
+    write_i2c(0b1101011, 0x10, 0b10000010); //communicate with ctrl1_xl and set the sample rate to 1.66 kHz (first four bits 1000), with 2g sensitivity (fifth and sixth bits to 00), and 100 Hz filter (last two bits to 10).
+    write_i2c(0b1101011, 0x11, 0b10001100); //communicate with ctrl2_g and set the sample rate to 1.66 kHz (first four bits 1000), with with 1000 dps sensitivity (5th and 6th bits to 11)
+
+}
+
+
+
+
+
+
+
 
 /* TODO:  Add any necessary callback functions.
  */
@@ -190,6 +397,7 @@ USB_DEVICE_CDC_EVENT_RESPONSE APP_USBDeviceCDCEventHandler
 /***********************************************
  * Application USB Device Layer Event Handler.
  ***********************************************/
+
 void APP_USBDeviceEventHandler(USB_DEVICE_EVENT event, void * eventData, uintptr_t context) {
     USB_DEVICE_EVENT_DATA_CONFIGURED *configuredEventData;
 
@@ -331,6 +539,27 @@ void APP_Initialize(void) {
     /* Set up the read buffer */
     appData.readBuffer = &readBuffer[0];
 
+    appData.state = APP_STATE_INIT;
+
+    __builtin_mtc0(_CP0_CONFIG, _CP0_CONFIG_SELECT, 0xa4210583);
+
+    // 0 data RAM access wait states
+    BMXCONbits.BMXWSDRM = 0x0;
+
+    // enable multi vector interrupts
+    INTCONbits.MVEC = 0x1;
+
+    // disable JTAG to get pins back
+    DDPCONbits.JTAGEN = 0;
+
+    TRISBbits.TRISB4 = 1; //pin B4 is an input
+    TRISAbits.TRISA4 = 0; //pin A4 is an output
+    LATAbits.LATA4 = 1; //set the initial output of pin A4 to high (3.3V)
+
+    LCD_init(); //initialize LCD
+    initExp(); //initialize LSM
+    LCD_clearScreen(BLACK); //turn the whole screen back
+
     startTime = _CP0_GET_COUNT();
 }
 
@@ -344,6 +573,27 @@ void APP_Initialize(void) {
 void APP_Tasks(void) {
     /* Update the application state machine based
      * on the current state */
+
+    unsigned char message [20];
+    unsigned char message1 [20]; //array that is going to contain the temperature (remember to save one extra character for the zero in sprint f)
+    unsigned char message2 [20];
+    unsigned char message3 [20];
+    unsigned char message4 [20];
+    unsigned char message5 [20];
+
+
+
+    unsigned char bar_number_x [4]; //array that is going to contain the number indicating the acc
+    signed short n = -10; //number that stores the progress of the progress bar
+    unsigned char data [30]; //array that stores the output data from the LSM
+
+    static unsigned int aa = 0;
+
+    /* TODO: Initialize your application's state machine and other
+     * parameters.
+     * 
+     * 
+     */
 
     switch (appData.state) {
         case APP_STATE_INIT:
@@ -362,6 +612,7 @@ void APP_Tasks(void) {
             }
 
             break;
+
 
         case APP_STATE_WAIT_FOR_CONFIGURATION:
 
@@ -390,6 +641,12 @@ void APP_Tasks(void) {
                         &appData.readTransferHandle, appData.readBuffer,
                         APP_READ_BUFFER_SIZE);
 
+
+                if (appData.readBuffer[0] == 'r') { //if the letter r is read by the computer
+                    aa = 1;
+                }
+
+
                 if (appData.readTransferHandle == USB_DEVICE_CDC_TRANSFER_HANDLE_INVALID) {
                     appData.state = APP_STATE_ERROR;
                     break;
@@ -408,7 +665,7 @@ void APP_Tasks(void) {
             /* Check if a character was received or a switch was pressed.
              * The isReadComplete flag gets updated in the CDC event handler. */
 
-            if (appData.isReadComplete || _CP0_GET_COUNT() - startTime > (48000000 / 2 / 5)) {
+            if (appData.isReadComplete || _CP0_GET_COUNT() - startTime > (48000000 / 2 / 100)) {
                 appData.state = APP_STATE_SCHEDULE_WRITE;
             }
 
@@ -416,6 +673,7 @@ void APP_Tasks(void) {
 
 
         case APP_STATE_SCHEDULE_WRITE:
+
 
             if (APP_StateReset()) {
                 break;
@@ -427,8 +685,60 @@ void APP_Tasks(void) {
             appData.isWriteComplete = false;
             appData.state = APP_STATE_WAIT_FOR_WRITE_COMPLETE;
 
-            len = sprintf(dataOut, "%d\r\n", i);
+            if (aa == 1) {
+                read_multiple_i2c(0b1101011, 0x20, data, 14); //read the seven consecutive registers indicating temperature, acceleration in xyz, and angular velocity in xyz
+
+                //signed short temp = data[1]<< 8 | data [0]; //temperature is given by two 8 bits number, store the second one, then shift on by 8 bits and store the first one
+                signed short gyro_x = data[3] << 8 | data [2];
+                signed short gyro_y = data[5] << 8 | data [4];
+                signed short gyro_z = data[7] << 8 | data [6];
+                signed short acc_x = data[9] << 8 | data [8];
+                signed short acc_y = data[11] << 8 | data [10];
+                signed short acc_z = data[13] << 8 | data [12];
+
+                //sprintf(message1, "%d" , temp);  
+
+                //writeString(10, 10, message1, WHITE, BLACK); //write temp starting from pixel at x=28 y=32
+
+                if (abs(acc_x) < 1000) {
+                    sprintf(message2, "Ax %d   ", acc_x);
+                } else {
+                    sprintf(message2, "Ax %d ", acc_x);
+                }
+                writeString(10, 20, message2, WHITE, BLACK);
+
+                if (abs(acc_y) < 1000) {
+                    sprintf(message3, "Ay %d   ", acc_y);
+                } else {
+                    sprintf(message3, "Ay %d ", acc_y);
+                }
+
+                writeString(10, 30, message3, WHITE, BLACK);
+
+                sprintf(message4, "Gx %d    ", gyro_x);
+
+                writeString(10, 40, message4, WHITE, BLACK);
+
+                sprintf(message5, "Gy %d    ", gyro_y);
+
+                writeString(10, 50, message5, WHITE, BLACK);
+
+
+                
+                
+                len = sprintf(dataOut, "%d Ax %d Ay %d Az %d Gx %d Gy %d Gz %d \r\n", i, acc_x, acc_y, acc_z, gyro_x, gyro_y, gyro_z);
             i++;
+            }
+            else {
+                len = 1;
+                dataOut[0] = 0;
+            }
+
+            if (i > 100) {
+                aa = 0;
+                i=0;
+            }
+            
             if (appData.isReadComplete) {
                 USB_DEVICE_CDC_Write(USB_DEVICE_CDC_INDEX_0,
                         &appData.writeTransferHandle,
